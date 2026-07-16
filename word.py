74e6665fbf69f4a3ef944420b0ce10ada8c98d9e
faiqimran589 <faiqimran589@gmail.com>
2026-07-17 00:44:57 +0500

    Update program logic

--- word.py
+++ word.py
@@ -15,27 +15,31 @@ words=[
 
 
 
-secret_words=random.choice(words)
-guessed_letters=[]
-attempts=6
-display_word=["_"]*len(secret_words)
-game_over=False
+secret_words=random.choice(words)       #store secret word which was chosen by program
+guessed_letters=[]                      #store guessed letter which was guessed by user
+attempts=10                             #number of attempts
+display_word=["_"]*len(secret_words)    #hidden numbers 
+game_over=False                         #controls games loop
 
 
-while attempts>0:
+while attempts>0:                       #for continuing the game until player guess or games end 
     guess=input('Enter you guess:')
-
+    for i in range(len(secret_words)):
+      if guess == secret_words[i]:
+        display_word[i]=guess
+        print('Secret_words:',"".join(display_word))
     if guess in secret_words:
-       print('corect guess')
+        print('corect guess')
     else:
       print('incorrect guess')
-      attempts-=1
+      attempts-=1                       #decreasing number of guesses
 
-    print('Secret_words:',"".join(display_word))
+    
     print("Attempts:",attempts)
 
 
     if "_" not in display_word:
       print("Congratulation")
-    if attempts==0:
+      break
+    if attempts == 0:
      print("the correct word is ",secret_words)
